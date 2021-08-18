7b9bc33db7a56a506a1d1c853571a46b92678dd7
nagibfiroz19 <nagibfiroz.seu19@gmail.com>
2021-08-18 18:26:42 +0600

    done

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -10,8 +10,29 @@ function updateProductNumber(product, price, isADD) {
     productInput.value = productNumber;
     const productTotal = document.getElementById(product + '-Total');
     productTotal.innerText = productNumber * price;
+    calculateTotal();
+}   
+
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
 }
-        //handle product increase decrease event
+
+function calculateTotal() {
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const totalPrice = subTotal + tax;
+    document.getElementById('sub-Total').innerText = subTotal;
+    console.log(subTotal);
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('Total-price').innerText = totalPrice;
+
+}
+
+//handle product increase decrease event
 
 document.getElementById('phone-plus').addEventListener('click', function () {
     updateProductNumber('phone', 1219, true);
@@ -19,7 +40,7 @@ document.getElementById('phone-plus').addEventListener('click', function () {
 document.getElementById('phone-minus').addEventListener('click', function () {
     updateProductNumber('phone', 1219, false);
 })
-        //handle case increase decrease event
+//handle case increase decrease event
 
 document.getElementById('case-plus').addEventListener('click', function () {
     updateProductNumber('case', 59, true);
